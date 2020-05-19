aa1f3468b2c2edab7597a2642c257b8e5b049b2e
guipernicone <guipernicone@hotmail.com>
2020-05-19 02:43:34 -0300

    #24 - Adiciona Projeto Spring Server

---
+++ Server/src/main/java/com/tcc/CrowdVision/CrowdVisionApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.CrowdVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdVisionApplication.class, args);
+	}
+
+}
