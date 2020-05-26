3a553d75024b6075128ce346bcf66d4350b528c8
guipernicone <guipernicone@hotmail.com>
2020-05-26 08:56:56 -0300

    Adiciona suporte para erro na rota de detecao

--- Server/src/main/java/com/tcc/CrowdVision/Controller/DetectionController.java
+++ Server/src/main/java/com/tcc/CrowdVision/Controller/DetectionController.java
@@ -1,6 +1,7 @@
 package com.tcc.CrowdVision.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +27,16 @@ public class DetectionController {
 		System.out.println(detection.getLongitude());
 		System.out.println(detection.getTime());
 		
-		Detection savedDetection = detectionRepository.save(detection);
-		System.out.println(savedDetection.toString());
+		try {
+			Detection savedDetection = detectionRepository.save(detection);
+			System.out.println(savedDetection.toString());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed trying to save on database");
+		}
+
 		return ResponseEntity.ok("Success");
 	}
 }
