7f9e7bc693a611923823dabc31e3b06dc78f8e85
guipernicone <guipernicone@hotmail.com>
2020-05-30 08:25:08 -0300

    volta comunicação com servidor

--- DetectionAPI/src/capture/video_capture.py
+++ DetectionAPI/src/capture/video_capture.py
@@ -45,12 +45,12 @@ def send_post(URL, payload):
     headers = {'content-type': 'application/json'}
     return requests.post(url = URL, data=json.dumps(payload), headers=headers)
 
-def send_detected_frame(frame, score, device_key):
+def send_detected_frame(detection, device_key):
     URL = "http://localhost:8080/detection/add-frame/detected"
 
     payload = {
-            "frame" : str(frame),
-            "detectionScore" : float(score),
+            "frame" : detection['frame'],
+            "detectionScore" : detection['score'],
             "cameraId": str(device_key),
             "time" : datetime.today().strftime('%d-%m-%Y %H:%M:%S')
         }
@@ -99,26 +99,38 @@ def send_to_detection(frame):
 #                         DETECTION
 #--------------------------------------------------------------------
 
-def validate_frame(image_np):
+def validate_frame(image_np, device_key):
+    global delay, initial_time
     jpg_img = convert_frame_to_base_64_string(image_np)
     response = send_to_detection(jpg_img);
     jsonResponse = json.loads(response)
     print(jsonResponse['score'])
-    # print(response.json())
-    # print(threading.current_thread())
-    # Display the frames with the detection boxes
-    # cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
 
-    # if cv2.waitKey(25) & 0xFF == ord('q'):
-    #     cv2.destroyAllWindows()
+    if (jsonResponse['score'] > 0.75):
+                print("Detectou");
+                detectionTime = int(round(time.time() * 1000))
+                running_time = detectionTime - initial_time
+                #Send the detection once 5 seconds
+                if (delay <= running_time):
+                    initial_time = int(round(time.time() * 1000))
+                    delay = 5000
+                    threaded = threading.Thread(target=send_detected_frame, args=(jsonResponse, device_key))
+                    threaded.daemon = True
+                    threaded.start()
+                    
+
 i = 0
+delay = 0
+initial_time = int(round(time.time() * 1000))
+device_key = register_deveice()
+print(device_key)
 while True:
     # Read a frame from the defined stream
     ret, image_np = cap.read()
 
     
     if (ret == True):
-        validate_frame(image_np)
+        validate_frame(image_np, device_key)
         # thread_id = thread.start_new_thread(validate_frame, (image_np,))
         # threaded = threading.Thread(target=validate_frame, args=(image_np,))
         # threaded.daemon = True  # This thread dies when main thread (only non-daemon thread) exits.
