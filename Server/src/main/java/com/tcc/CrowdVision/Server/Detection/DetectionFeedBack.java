c9513c939cb7a1c4107061b332c220abcd58ab93
guipernicone <guipernicone@hotmail.com>
2020-06-10 08:22:08 -0300

    Renomeia classe

--- Server/src/main/java/com/tcc/CrowdVision/Server/Detection/DetectionFeedBack.java
+++ Server/src/main/java/com/tcc/CrowdVision/Server/Detection/DetectionFeedBack.java
@@ -3,8 +3,8 @@ package com.tcc.CrowdVision.Server.Detection;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "DetectionFeedback")
-public class DetectionFeedback {
+@Document(collection = "DetectionFeedBack")
+public class DetectionFeedBack {
 	@Id
 	private String id;
 	private String detectionId;
