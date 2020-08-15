b69c764b19f1b67c6a7af904958be57a326a85ae
guipernicone <guipernicone@hotmail.com>
2020-08-15 03:08:53 -0300

    refatora condição de reload

--- Interface/src/Page/Navbar/Navbar.jsx
+++ Interface/src/Page/Navbar/Navbar.jsx
@@ -6,6 +6,12 @@ import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
     const location = useLocation()
+
+    const verifyStatisticsPage = () => {
+        if (location.pathname == '/statistics') {
+            window.location.reload()
+        }
+    }
     return (
         <NavbarStyle>
             <div className= "navBarGroup1">
@@ -14,7 +20,7 @@ const Navbar = () => {
                 {console.log(Math.random())}
                 <Link to={'/'}><div className="navButton">Detecções</div></Link>
                 <Link to={'/history'} ><div className="navButton">Histórico</div></Link>
-                <Link to={'/statistics'} onClick={() => location.pathname == '/statistics' ? window.location.reload() : null}><div className="navButton">Estatísticas</div></Link>
+                <Link to={'/statistics'} onClick={verifyStatisticsPage}><div className="navButton">Estatísticas</div></Link>
                 <Link to={'/camera'} ><div className="navButton">Câmeras</div></Link>
             </div>
             <div className="navBarGroup2">
