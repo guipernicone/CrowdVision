274e638d1fce24131ec70b5d2829bec1dbd237de
guipernicone <guipernicone@hotmail.com>
2020-08-27 05:38:39 -0300

    Adiciona converção por hsv

--- DetectionAPI/src/capture/video_capture.py
+++ DetectionAPI/src/capture/video_capture.py
@@ -41,12 +41,18 @@ def convert_frame_to_base_64_string(frame):
     return base64.b64encode(buffer)
 
 def convert_color_img_to_black_and_white(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # threshold the image
-    img_binary = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
+    # # threshold the image
+    # img_binary = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
-    return img_binary
+    lower_black = np.array([0, 0, 0], np.uint8)
+    upper_black = np.array([179, 100, 200], np.uint8)
+    
+    mask = cv2.inRange(hsv, lower_black, upper_black)
+    res = cv2.bitwise_and(image,image, mask= mask)
+    return res
 
 
 #--------------------------------------------------------------------
@@ -155,7 +161,7 @@ if (len(sys.argv) >= 3 and sys.argv[1] == "-camera_name"):
         # Read a frame from the defined stream
         ret, image_np = cap.read()
 
-        # image_np = convert_color_img_to_black_and_white(image_np)
+        image_np = convert_color_img_to_black_and_white(image_np)
         
         if (failed_send_attemps > 10):
             failed_send_attemps = 0
